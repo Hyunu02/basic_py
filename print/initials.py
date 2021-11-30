e9d2e9586272d1a6ac9242444d998c0a6d8ce85d
Hyunu02 <andrehwkc@gmail.com>
2021-11-29 21:53:04 -0300

    Add print and initial print!

---
+++ print/initials.py
@@ -0,0 +1,9 @@
+# Code by Andre Cho
+
+print("  A     CCC ")
+print(" A A   C   C")
+print("A   A  C    ")
+print("AAAAA  C    ")
+print("A   A  C    ")
+print("A   A  C   C")
+print("A   A   CCC")
---
+++ print/print.py
@@ -0,0 +1,20 @@
+# there is different ways to print thing you want to show
+
+# 1 method
+
+print("Hey my name is %s." % "Andre")
+print("I have %d pencils and %d papers!" %(2, 3))
+
+# 2 method
+
+print("I like {} color and {} color.".format("blue", "red"))
+print("I like {1} and {0}.".format("mango", "watermelon"))
+
+# 3 method
+
+print("I have a doggo called {name} and she is {age}".format(name = "Hyba", age = 7))
+
+# 4 method, v3.6 or higher
+
+present = "a camera"
+print(f"What did you really give me {present}?")
