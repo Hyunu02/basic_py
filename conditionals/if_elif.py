17ac2cd016a037606eaf548bbd97c2a9843d39d5
Hyunu02 <andrehwkc@gmail.com>
2021-11-29 22:13:37 -0300

    Conditionals in python

---
+++ conditionals/if_elif.py
@@ -0,0 +1,12 @@
+x = 0
+ 
+if x == 0:
+  print("x is equal to zero.")
+elif x >= 0:
+  print("x is greater than zero.")
+else:
+  print("x is less than zero.")
+
+likes_snakes = input("Do you like snakes? ")
+print(likes_snakes)
+print(1==1)
