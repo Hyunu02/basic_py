208526be90e87851bcc959c525cfeb84c03b3bbe
Hyunu02 <andrehwkc@gmail.com>
2021-12-02 17:19:58 -0300

    Practiced for loop

---
+++ loops/for.py
@@ -0,0 +1,24 @@
+# for <temporary variable> in <collection>:
+#   <action>
+
+# <temporary variable> --> represent the value of the element in the
+# collection the loop is currently on. Does not need to be defined beforehand
+# best practice: make it as descriptive as possible
+
+# <collection> --> to loop over
+
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+for game in board_games:
+  print(game)
+print("\n")
+
+# using range() built-in function
+
+for i in range(5):
+  print(board_games[i])
+
+phrase = "Iteration number: "
+
+for temp in range(5):
+  print(phrase + str(temp + 1))
