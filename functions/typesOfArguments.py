e55b43b89769743861a55414d5a00a3539b01c57
Hyunu02 <andrehwkc@gmail.com>
2021-12-03 16:31:34 -0300

    Practiced types of arguments, scope and multiple returns

---
+++ functions/typesOfArguments.py
@@ -0,0 +1,16 @@
+# 1) Positional arguments: depend on their positions in function call
+# 2) Keyword arguments: refer explicitly to what each argument is assigned when call function
+#  calculate_taxi_price(rate=0.5, discount = 10, miles_to_travel = 100)
+# 3) Default arguments: can set default argument
+#  def calculate_taxi_price(miles_to_travel, rate, discount = 10)
+
+def trip_planner(first_destination, second_destination, final_destination = "Codecademy HQ"):
+  print("Here is what your trip will look like!")
+  print("First, we will stop in " + first_destination + ", then " + second_destination + ", and lastly " + final_destination)
+
+# Keyword argument
+trip_planner(first_destination = "Iceland", final_destination = "Germany", second_destination = "India")
+# third argument as default in function
+trip_planner("Brooklyn", "Queens")
+
+# scope --> name where variable can be accessed
