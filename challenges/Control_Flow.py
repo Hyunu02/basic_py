106ea4af09848221f703e559546f82ecd56921eb
Hyunu02 <andrehwkc@gmail.com>
2021-12-06 13:57:45 -0300

    Twice as large, Divisible by ten, Not sum to ten challenges done

--- challenges/Control_Flow.py
+++ challenges/Control_Flow.py
@@ -13,3 +13,21 @@ def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
     sum = food_bill + electricity_bill + internet_bill + rent
     if budget < sum: return True
     return False
+
+# Twice As Large
+# CHECKS IF NUM1 IS TWICE AS LARGE THAN NUM2
+def twice_as_large(num1, num2):
+    if num1 > num2 * 2: return True
+    return False
+
+# Divisible By Ten
+# VERIFIES IF NUM IS DIVISIBLE BY TEN
+def divisible_by_ten(num):
+    if num % 10 == 0: return True
+    return False
+
+# Not Sum To Ten
+# CHECK IF SUMMATION OF TWO INPUTS DOESN'T EQUAL TEN
+def not_sum_to_ten(num1, num2):
+    if (num1 + num2 != 10): return True
+    return False
